780363fef3c560635b3c77e547b839a0fbc443ac
CDFN <codefun@protonmail.com>
2021-01-24 23:21:47 +0100

    Revert "Add callback for disconnecting from server"
    This reverts commit 2f724baf

--- src/main/java/pl/cdfn/fabricord/mixin/ServerJoinMixin.java
+++ src/main/java/pl/cdfn/fabricord/mixin/ServerJoinMixin.java
@@ -2,18 +2,16 @@ package pl.cdfn.fabricord.mixin;
 
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import pl.cdfn.fabricord.callback.ServerDisconnectCallback;
 import pl.cdfn.fabricord.callback.ServerJoinCallback;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public abstract class ServerConnectionMixin {
+public abstract class ServerJoinMixin {
 
   @Shadow
   public abstract ClientConnection getConnection();
@@ -23,8 +21,4 @@ public abstract class ServerConnectionMixin {
     ServerJoinCallback.EVENT.invoker().serverJoin(this.getConnection().isLocal(), packet.getDimensionId().getValue().getPath());
   }
 
-  @Inject(method = "onDisconnect", at = @At("RETURN"))
-  private void onDisconnect(DisconnectS2CPacket packet, CallbackInfo callbackInfo) {
-    ServerDisconnectCallback.EVENT.invoker().serverDisconnect();
-  }
 }
