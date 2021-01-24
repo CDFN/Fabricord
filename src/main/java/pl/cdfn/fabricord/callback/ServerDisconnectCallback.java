2f724baf4efd0cbb1668be90bb1fb2f3589f7817
CDFN <codefun@protonmail.com>
2021-01-24 23:05:20 +0100

    Add callback for disconnecting from server

---
+++ src/main/java/pl/cdfn/fabricord/callback/ServerDisconnectCallback.java
@@ -0,0 +1,14 @@
+package pl.cdfn.fabricord.callback;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+public interface ServerDisconnectCallback {
+  Event<ServerDisconnectCallback> EVENT = EventFactory.createArrayBacked(ServerDisconnectCallback.class, handlers -> () -> {
+    for (ServerDisconnectCallback handler : handlers) {
+      handler.serverDisconnect();
+    }
+  });
+
+  void serverDisconnect();
+}
--- src/main/java/pl/cdfn/fabricord/mixin/ServerConnectionMixin.java
+++ src/main/java/pl/cdfn/fabricord/mixin/ServerConnectionMixin.java
@@ -2,16 +2,18 @@ package pl.cdfn.fabricord.mixin;
 
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pl.cdfn.fabricord.callback.ServerDisconnectCallback;
 import pl.cdfn.fabricord.callback.ServerJoinCallback;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public abstract class ServerJoinMixin {
+public abstract class ServerConnectionMixin {
 
   @Shadow
   public abstract ClientConnection getConnection();
@@ -21,4 +23,8 @@ public abstract class ServerJoinMixin {
     ServerJoinCallback.EVENT.invoker().serverJoin(this.getConnection().isLocal(), packet.getDimensionId().getValue().getPath());
   }
 
+  @Inject(method = "onDisconnect", at = @At("RETURN"))
+  private void onDisconnect(DisconnectS2CPacket packet, CallbackInfo callbackInfo) {
+    ServerDisconnectCallback.EVENT.invoker().serverDisconnect();
+  }
 }
