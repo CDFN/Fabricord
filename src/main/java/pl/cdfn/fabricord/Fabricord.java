07eb5956b9ed673a873231e592f8063a7124cf69
CDFN <codefun@protonmail.com>
2021-01-24 15:56:59 +0100

    Implement basic features

--- src/main/java/pl/cdfn/fabricord/Fabricord.java
+++ src/main/java/pl/cdfn/fabricord/Fabricord.java
@@ -1,8 +1,11 @@
 package pl.cdfn.fabricord;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 import net.fabricmc.api.ModInitializer;
 
 public class Fabricord implements ModInitializer {
+  public static final Logger LOGGER = LogManager.getLogManager().getLogger("Fabricord");
 
   @Override
   public void onInitialize() {}
---
+++ src/main/java/pl/cdfn/fabricord/discord/AssetKey.java
@@ -0,0 +1,38 @@
+package pl.cdfn.fabricord.discord;
+
+/**
+ * Determines asset keys defined in application in https://discord.com/developers/
+ * Non-default applications should follow names defined in these classes.
+ */
+public enum AssetKey {
+    LOGO("logo"),
+    MISSING("missing");
+
+    private final String key;
+
+    AssetKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public enum Dimension {
+        UNKNOWN(MISSING.getKey()),
+        OVERWORLD("overworld"),
+        THE_NETHER("the_nether"),
+        THE_END("the_end");
+
+        private static final String PREFIX = "dimension_";
+        private final String key;
+
+        Dimension(String key) {
+            this.key = PREFIX + key;
+        }
+
+        public String getKey() {
+            return key;
+        }
+    }
+}
